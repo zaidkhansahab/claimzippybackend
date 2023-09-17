bbf1d4ce85f19fd565339586e85e059fded10e25
MOHD ZAID KHAN <100786733+zaidkhansahab@users.noreply.github.com>
2023-09-17 17:20:14 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ const routes = require("./routes/taskroutes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT ;
 
 app.use(express.json());
 app.use(cors());
